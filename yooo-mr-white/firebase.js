b96bf1944c60b9fdf10426a2bb9ee48fcf041157
pawelcp <94795184+pawelcp@users.noreply.github.com>
2023-01-05 00:10:43 +0100

    readytowrite

---
+++ yooo-mr-white/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCSHHSoAKuqTG7G9hpyc_evNvhHuefYyTc",
+  authDomain: "fakeshop-a5c15.firebaseapp.com",
+  projectId: "fakeshop-a5c15",
+  storageBucket: "fakeshop-a5c15.appspot.com",
+  messagingSenderId: "21695359296",
+  appId: "1:21695359296:web:3487cfefb30cd949b0b2a5",
+  measurementId: "G-23ZTBDC775"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
