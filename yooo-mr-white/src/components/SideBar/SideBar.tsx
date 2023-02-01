42ada80086813600762c3929a60fa19cad2ba156
Yamcz00r <99492855+Yamcz00r@users.noreply.github.com>
2023-02-01 21:45:52 +0100

    implement-signout-functionality

--- yooo-mr-white/src/components/SideBar/SideBar.tsx
+++ yooo-mr-white/src/components/SideBar/SideBar.tsx
@@ -1,12 +1,16 @@
-import { useState } from "react";
-import { Text, Flex, Avatar } from "@chakra-ui/react";
+import { Text, Flex, Avatar, Button, useToast } from "@chakra-ui/react";
 import styles from "./SideBar.module.css";
 import { IoMdClose } from "react-icons/io";
+import { BiLogOut } from "react-icons/bi";
 import { IconContext } from "react-icons";
 import SideBarItem from "./SideBarItem";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { logout } from "../../../store/userSlice";
+import { signOut } from "firebase/auth";
+import { auth } from "../../../firebase";
 import { selectUser } from "../../../store/userSlice";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 interface SideBarProps {
   isOpen: boolean;
@@ -36,6 +40,25 @@ const SideBar = ({ isOpen, onSlideOut }: SideBarProps) => {
   const user = useSelector(selectUser);
   const transitionProperties =
     isOpen === true ? { marginLeft: "0px" } : { marginLeft: "-300px" };
+  const dispatch = useDispatch();
+  const toast = useToast();
+  const router = useRouter();
+
+  const logoutHandler = async () => {
+    try {
+      await signOut(auth);
+      dispatch(logout());
+    } catch (error) {
+      toast({
+        title: "Failed to sign out!.",
+        description: "Something went wrong. Sorry !",
+        status: "error",
+        duration: 9000,
+        isClosable: true,
+      });
+    }
+  };
+
   return (
     <aside style={transitionProperties} className={styles["side-bar"]}>
       <Flex
@@ -80,35 +103,73 @@ const SideBar = ({ isOpen, onSlideOut }: SideBarProps) => {
           gap="4"
           alignItems="center"
           justifyContent="center"
-          marginTop="44"
+          marginTop="32"
         >
-          <Link
-            href="/auth/login"
+          <Button
+            onClick={() => router.push("/auth/login")}
+            size="lg"
+            background="#2D2A43"
+            _hover={{ background: "transparent" }}
             className={`${styles.button} ${styles.login}`}
           >
             Log In
-          </Link>
-          <Link
-            href="/auth/register"
+          </Button>
+          <Button
+            onClick={() => router.push("/auth/register")}
+            size="lg"
+            background="transparent"
+            _hover={{ background: "#2D2A43" }}
             className={`${styles.button} ${styles.register}`}
           >
             Register
-          </Link>
+          </Button>
         </Flex>
       ) : (
         <Flex
-          paddingY="3"
+          paddingY="2"
           className={styles["side-bar-item"]}
           w="full"
           gap="4"
           alignItems="center"
           justifyContent="center"
-          marginTop="44"
+          marginTop="32"
         >
-          <Avatar size="md" />
+          <Avatar size="sm" />
           <Text as="b" fontSize="lg">
             {user?.email}
           </Text>
+        </Flex>
+      )}
+      {!user ? (
+        ""
+      ) : (
+        <Flex
+          marginTop="10"
+          w="full"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <Button
+            onClick={logoutHandler}
+            size="lg"
+            background="transparent"
+            _hover={{ background: "#2D2A43" }}
+            className={`${styles.button} ${styles.logout}`}
+          >
+            <Flex
+              w="full"
+              h="full"
+              justifyContent="space-between"
+              alignItems="center"
+            >
+              <Text as="b" fontSize="xl" color="inherit">
+                Logout
+              </Text>
+              <IconContext.Provider value={{ size: "30px", color: "inherit" }}>
+                <BiLogOut />
+              </IconContext.Provider>
+            </Flex>
+          </Button>
         </Flex>
       )}
     </aside>
